8f9a534f5bc520c4cd3b95ebb5f91baccbd865f0
Maryna Netrebchuk <marynanetrebchuk@Marynas-MacBook-Air.local>
2022-11-22 16:37:21 -0500

    Add element to header

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -10,7 +10,6 @@ import { CountryDetailComponent } from './country-detail/country-detail.componen
 import { CountryCardComponent } from './country-card/country-card.component';
 import { FormsModule } from '@angular/forms';
 import { CountryFilterComponent } from './country-filter/country-filter.component';
-
 @NgModule({
   declarations: [
     AppComponent,
@@ -27,6 +26,7 @@ import { CountryFilterComponent } from './country-filter/country-filter.componen
     AppRoutingModule
   ],
   providers: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
